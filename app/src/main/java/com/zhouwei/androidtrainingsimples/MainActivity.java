106ffb8d0b9c2ccb12ce631ba12de44d9285be00
wzq <wzqjava@163.com>
2019-09-10 20:52:04 +0800

    test2

--- app/src/main/java/com/zhouwei/androidtrainingsimples/MainActivity.java
+++ app/src/main/java/com/zhouwei/androidtrainingsimples/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
   // oncreate+
+  //test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
