7edb305022613490507895776c96efe79aa57321
wzq <wzqjava@163.com>
2019-09-10 20:44:50 +0800

    增加注释oncreate

--- app/src/main/java/com/zhouwei/androidtrainingsimples/MainActivity.java
+++ app/src/main/java/com/zhouwei/androidtrainingsimples/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  // oncreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
